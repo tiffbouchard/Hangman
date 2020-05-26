98cb4dfbe922c54ad9fcb22bb112d2bccd4c75da
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-05-26 12:10:19 -0400

    Complete CSS styling

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 /*----- constants -----*/
 
-
-
 /*----- app's state (variables) -----*/
 
 
 
 /*----- cached element references -----*/
 
+let buttons = document.querySelectorAll("button.alph");
 
 
 /*----- event listeners -----*/
 
-
+buttons.forEach(function(button){button.addEventListener('click', function() {console.log("clicked")})})
 
 /*----- functions -----*/
