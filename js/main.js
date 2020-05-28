3d16e094d6676f14de0a18f8bd82a402f8218ea8
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-05-28 12:22:50 -0400

    Update renderLetterMatch function

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const words = [
   "camping",
   "travelling",
 ];
-const maxMistakes = 9;
+const maxMistakes = 9; 
 
 /*----- app's state (variables) -----*/
 
@@ -19,15 +19,22 @@ let mistakesMade;
 let wordsCompleted = [];
 let wordSplit = [];
 let hiddenWord = [];
+let currentWords = [];
 
 /*----- cached element references -----*/
 
 let buttons = document.getElementById("keyboard");
 let playAgain = document.getElementById("play-again");
+let playAgainBtn = document.querySelector("#play-again button");
+playAgainBtn.disabled = true;
 let hangman = document.getElementById("diagram");
 let wordToGuess = document.getElementById("word-to-guess");
 let hiddenWordEl = document.getElementById("hidden-word");
+let startButton = document.getElementById("start");
+let msg = document.getElementById("msg");
 
+
+//hangman
 let base = document.getElementById("base");
 let poleOne = document.getElementById("pole1");
 let poleTwo = document.getElementById("pole2");
@@ -39,13 +46,16 @@ let leg1 = document.getElementById("leg1");
 let leg2 = document.getElementById("leg2");
 
 /*----- event listeners -----*/
-
+startButton.addEventListener("click", start);
 buttons.addEventListener("click", handleClick);
 playAgain.addEventListener("click", init);
 
 /*----- functions -----*/
 
-init();
+//Have full hangman drawing visible, no secret word visible
+//show Message: READY TO PLAY? 
+//have button to click that says: START!
+//START button will remove the START button and the message and run the init() function;
 
 function handleClick(evt) {
   if (evt.target.id === "keyboard") {
@@ -56,11 +66,15 @@ function handleClick(evt) {
   if (lettersGuessed.includes(letterGuessed)) {
     evt.target.disabled = true;
   }
-  console.log(lettersGuessed);
+  renderLetterMatch();
 }
 
 function renderRandomWord() {
   word = words[Math.floor(Math.random() * words.length)];
+  word = word.toUpperCase();
+  currentWords.push(word);
+  //never render the same word, if all words are used up 
+  wordSplit = word.split("");
   for (var i = 0; i < word.length; i++) {
     hiddenWord.push("_");
   }
@@ -73,14 +87,76 @@ function renderRandomWord() {
 // hiddenWordEl.style.visibility = "hidden";
 
 function renderLetterMatch() {
-  //compare letters from the wordSplit, using
-  //compare evt.target to word
+  // showLetter = wordSplit.includes(letterGuessed);
+  if (wordSplit.includes(letterGuessed) === true) {
+    msg.textContent = "YOU GOT IT!";
+    msg.style.visibility = "visible";
+  } else if (wordSplit.includes(letterGuessed) === false) {
+    msg.textContent = "TRY AGAIN!";
+    msg.style.visibility = "visible";
+    mistakesMade++;
+  }if (mistakesMade === 1) {
+    base.style.visibility = "visible"
+  } if (mistakesMade === 2) {
+    poleOne.style.visibility = "visible"
+  } if (mistakesMade === 3) {
+    poleTwo.style.visibility = "visible"
+  } if (mistakesMade === 4) {
+    poleThree.style.visibility = "visible"
+  } if (mistakesMade === 5) {
+    head.style.visibility = "visible"
+  } if (mistakesMade === 6) {
+    body.style.visibility = "visible"
+  } if (mistakesMade === 7) {
+    arms.style.visibility = "visible"
+  } if (mistakesMade === 8) {
+    leg1.style.visibility = "visible"
+  } if (mistakesMade === 9) {
+    leg2.style.visibility = "visible"
+  }
+} 
+
+
+// if (mistakesMade = maxMistakes) {
+//   msg.textContent = "YOU'RE OUT OF TRIES";
+//   msg.style.visibility = "visible";
+//where to put this^^^
+  
+  //display only those element from wordSplit and show CONGRATS msg (use split to )
+  //if it does not contain, mistakesMade++ and show TRY AGAIN msg
+  //if mistakesMade => maxMistakes end game and show BETTER LUCK NEXT TIME msg 
+  //if all letters are matched, and word is completed, push it to wordsCompleted
+  //
   //if word is successfully matched add to wordsCompleted
-}
+
+//how to not repeat self everytime
 
 function init() {
   renderRandomWord();
   hangman.style.visibility = "hidden";
+  base.style.visibility = "hidden"
+  poleOne.style.visibility = "hidden"
+  poleTwo.style.visibility = "hidden"
+  poleThree.style.visibility = "hidden"
+  head.style.visibility = "hidden"
+  body.style.visibility = "hidden"
+  arms.style.visibility = "hidden"
+  leg1.style.visibility = "hidden"
+  leg2.style.visibility = "hidden"
+  mistakesMade = 0;
+  wordsCompleted = [];
+  lettersGuessed = [];
+  letterGuessed;
+  hiddenWord = [];
+}
+//find a way to not repeat hidden everytime 
+
+function start() {
+  renderRandomWord();
+  playAgainBtn.disabled = false;
+  hangman.style.visibility = "hidden";
+  startButton.remove();
+  msg.style.visibility = "hidden";
   mistakesMade = 0;
   wordsCompleted = [];
   lettersGuessed = [];
@@ -98,5 +174,7 @@ function init() {
 //mistakesMade++
 //when mistakes > maxMistakes return; and display BETTER LUCK NEXT TIME!
 
-//make a secretword that is just dashes....make this display
-//
+
+//when start button is pressed, make transition more smooth, take like 2 seconds to dissapear
+
+//add theme somewhere on the page
